f50b7d6de1a58b321b6ff2e801166364fdf1cf20
gaurangrshah <gaurang.r.shah@gmail.com>
2019-12-30 21:54:20 -0500

    adds debounce to utils

---
+++ utils.js
@@ -0,0 +1,22 @@
+
+export function debounce(func, wait = 20, immediate = true) {
+  /*
+ usage: debounce(anyFunction)
+*/
+
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      // console.log('debounced')
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
